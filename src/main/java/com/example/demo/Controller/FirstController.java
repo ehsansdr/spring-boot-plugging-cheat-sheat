36f140dce37dad9abbcfdf77412230b1b6f8a04d
ehsan <sehsan251@gmail.com>
2025-01-19 10:41:34 +0330

    extra responce entity tutorial

--- src/main/java/com/example/demo/Controller/FirstController.java
+++ src/main/java/com/example/demo/Controller/FirstController.java
@@ -1,6 +1,9 @@
 package com.example.demo.Controller;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
@@ -37,5 +40,31 @@ public class FirstController {
         LOGGER.info("FirstController.class sayHello4 POST /post-order");
         return "request accepted and the message is " + order.toString();
     }
+
+    @GetMapping("/responce-entity")
+    public ResponseEntity<String> sayHello5() {
+        return new ResponseEntity<>("Hello form y first controller", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/responce-entity-header")
+    public ResponseEntity<String> sayHello9() {
+        String message = "this request must have header and status and body";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("1st pram", "2nd pram");// key - value
+
+        return new ResponseEntity<>(message,headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/is-success")
+    public ResponseEntity<String> sayHello10(@RequestParam boolean isSuccess) {
+        LOGGER.info("FirstController.class sayHello10() GET /is-success");
+        if (isSuccess) {
+            return new ResponseEntity<>("Request is successful", HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("Request is not successful", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
 
 }
--- src/main/java/com/example/demo/MySpringPluginProjectApplication.java
+++ src/main/java/com/example/demo/MySpringPluginProjectApplication.java
@@ -15,25 +15,20 @@ public class MySpringPluginProjectApplication {
 	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(MySpringPluginProjectApplication.class, args);
 
-		/*
 		// defining profile by code
-		var app = new SpringApplication(MySpringPluginProjectApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
-		var ctx = app.run(args);
+//		var app = new SpringApplication(MySpringPluginProjectApplication.class);
+//		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
+//		var ctx = app.run(args);
 
 		// bean creating
 		// var ctx = SpringApplication.run(MySpringPluginProjectApplication.class, args);
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println("MyFirstService: " + myFirstService.tellAStory());
-		System.out.println("readenv :" + myFirstService.readenv());
-		System.out.println("server port : " + myFirstService.ServerPort());
-		System.out.println("fileSep : " + myFirstService.fileSep());
-		System.out.println("os arc : " + myFirstService.osArch());
-		System.out.println("os version : " + myFirstService.osVersion());
+//		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
+//		System.out.println("MyFirstService: " + myFirstService.tellAStory());
+//		System.out.println("readenv :" + myFirstService.readenv());
+//		System.out.println("server port : " + myFirstService.ServerPort());
+//		System.out.println("fileSep : " + myFirstService.fileSep());
+//		System.out.println("os arc : " + myFirstService.osArch());
+//		System.out.println("os version : " + myFirstService.osVersion());
 		//System.getProperties().forEach((k, v) -> LOG.info("{} -> {}", k, v));
-		*/
-
 	}
-
-
 }
